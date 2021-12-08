56290dcefe4d597d2a29f275be1c73f8cd963f00
yathin <yathin@valorpayteh.com>
2021-12-08 18:44:20 +0530

    push changes

--- Backend/app.ts
+++ Backend/app.ts
@@ -15,3 +15,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
 });
+
+// {
+//     "src": "./Frontend/package.json",
+//     "use": "@vercel/static"
+//   }

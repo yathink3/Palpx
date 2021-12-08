8c2f1ef802fc5e635daeb3b9ae239d2f13566244
yathin <yathin@valorpayteh.com>
2021-12-08 16:50:53 +0530

    first commit

---
+++ Backend/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/home', (request: Request, response: Response) => {
+  console.log(request.url);
+  response.json({ message: `Welcome to the home page!` });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
